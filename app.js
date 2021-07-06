1333c259c9348bbd3dde425d0c1e9c88c0b38577
Nolo Marsh <nolo.marsh@gmail.com>
2021-07-06 19:52:08 -0400

    Functionality to produce items as divs working, going to try turning it into tables for smoother layout

--- app.js
+++ app.js
@@ -4,25 +4,35 @@ $( () => {
         cr: undefined,
     }
 
+    const addSearchResult = (monster) => {
+        $div = $('<div>').addClass('searchResult').appendTo($('#searchResults'))
+        $name = $('<p>').text(monster.name).appendTo($div)
+        $cr = $('<p>').text(monster.challenge_rating).appendTo($div)
+        $add = $('<div>').text('Add to Encounter').addClass('addBtn').appendTo($div)
+    }
+
+    $('#chooseCR').submit( (e) => {
+        e.preventDefault()
+        console.log($('#crDrop').val());
+    })
+
     $('#crBtn').click( () => {
         $('#crOptions').show()
     })
 
-     $('button').click( (e) => {
-        e.preventDefault()
-
-        $.ajax({
-            url:'https://api.open5e.com/monsters/',
-            data: {
-                limit : 50,
-                page: 1,
-                document__slug: 'wotc-srd',
-            }
-        }).then( (data) => {
-            console.log(data);
-        },
-        () => {
-            console.log('bad request');
-        })
+     $.ajax({
+         url:'https://api.open5e.com/monsters/',
+         data: {
+             limit : 50,
+             page: 1,
+             document__slug: 'wotc-srd',
+         }
+     }).then( (data) => {
+         for (let monster of data.results) {
+             addSearchResult(monster)
+         };
+     },
+     () => {
+         console.log('bad request');
      })
 })
